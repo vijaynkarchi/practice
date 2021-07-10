8e7f101e14fcf53e2b521cf63f96b77fcdd18392
Vijay Karchi <vijaynkarchi@icloud.com>
2021-07-10 16:38:53 +0530

    Day10 Q3 and Q1 solved

--- DSA/src/com/practice/vijay/AssignmentRunner.java
+++ DSA/src/com/practice/vijay/AssignmentRunner.java
@@ -7,9 +7,9 @@ public class AssignmentRunner {
     public static void main(String[] args) {
         // Create the instance of class and run execute method
         D010_Arrays_Jul_05 day10 = new D010_Arrays_Jul_05();
-        Arrays.stream(day10.printPattern(4)).toList().forEach(value -> {
-            Arrays.stream(value).forEach(System.out::print);
-            System.out.println();
-        });
+        // Q1
+        System.out.println(day10.countOfAG("ABCGAGG"));
+        // Q3
+//        Arrays.stream(day10.printPattern(1));
     }
 }
--- DSA/src/com/practice/vijay/D010_Arrays_Jul_05.java
+++ DSA/src/com/practice/vijay/D010_Arrays_Jul_05.java
@@ -3,6 +3,80 @@ package com.practice.vijay;
 public class D010_Arrays_Jul_05 {
 
     /**
+     * Question 1
+     * Special Subsequences "AG"
+     * Problem Description
+     *
+     * You have given a string A having Uppercase English letters.
+     *
+     * You have to find that how many times subsequence "AG" is there in the given string.
+     *
+     * NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+     *
+     *
+     *
+     * Problem Constraints
+     * 1 <= length(A) <= 105
+     *
+     *
+     *
+     * Input Format
+     * First and only argument is a string A.
+     *
+     *
+     *
+     * Output Format
+     * Return an integer denoting the answer.
+     *
+     *
+     *
+     * Example Input
+     * Input 1:
+     *
+     *  A = "ABCGAG"
+     * Input 2:
+     *
+     *  A = "GAB"
+     *
+     *
+     * Example Output
+     * Output 1:
+     *
+     *  3
+     * Output 2:
+     *
+     *  0
+     *
+     *
+     * Example Explanation
+     * Explanation 1:
+     *
+     *  Subsequence "AG" is 3 times in given string
+     * Explanation 2:
+     *
+     *  There is no subsequence "AG" in the given string.
+     *
+     *
+     *
+     * ×
+     * You only need to implement the given function. Do not read input, instead use the arguments to the function. Do not print the output, instead return values as specified. Still have a doubt? Checkout Sample Codes for more details.
+     */
+    public int countOfAG(String A) {
+        char[] chars = A.toCharArray();
+        int count = 0;
+        int ans = 0;
+        for(int i = (chars.length-1); i >= 0; i--) {
+            if(chars[i] == 'G') {
+                count++;
+            }
+            if(chars[i] == 'A') {
+                ans += count;
+            }
+        }
+        return ans;
+    }
+
+    /**
      * Question 3
      * Print a Pattern According to The Given Value of A.
      *
@@ -66,14 +140,21 @@ public class D010_Arrays_Jul_05 {
      *  Row 1 contains 3 spaces and 1 integer so spaces will be considered as 0 in the output.
      */
     public int[][] printPattern(int limit) {
-        if(limit == 1) return new int[][] {{1}};
-
         int[][] result = new int[limit][limit];
-        for(int i = 0; i > limit; i++ ) {
-            for(int j = limit-1; j >= limit-1+i; j-- ) {
-                result[i][j] = j;
+
+        for(int i = 0; i < limit; i++ ) {
+            int num = 1;
+            int count = 0;
+            for(int j = limit-1; j >= 0; j--) {
+                if(count <= i) {
+                    count++;
+                    result[i][j] = num++;
+                }
+                if(count > i) break;
             }
         }
         return result;
     }
+
+
 }
