53dac3f058d560d2fc5da9b5939d80a8cb188958
Vijay Karchi <vijaynkarchi@icloud.com>
2021-07-10 15:30:47 +0530

    First Commit

---
+++ DSA/src/com/practice/vijay/AssignmentRunner.java
@@ -0,0 +1,15 @@
+package com.practice.vijay;
+
+import java.util.Arrays;
+
+public class AssignmentRunner {
+
+    public static void main(String[] args) {
+        // Create the instance of class and run execute method
+        D010_Arrays_Jul_05 day10 = new D010_Arrays_Jul_05();
+        Arrays.stream(day10.printPattern(4)).toList().forEach(value -> {
+            Arrays.stream(value).forEach(System.out::print);
+            System.out.println();
+        });
+    }
+}
---
+++ DSA/src/com/practice/vijay/D010_Arrays_Jul_05.java
@@ -0,0 +1,79 @@
+package com.practice.vijay;
+
+public class D010_Arrays_Jul_05 {
+
+    /**
+     * Question 3
+     * Print a Pattern According to The Given Value of A.
+     *
+     * Example: For A = 3 pattern looks like:
+     *
+     *     1
+     *   2 1
+     * 3 2 1
+     *
+     * Problem Constraints
+     * 1 <= A <= 1000
+     *
+     *
+     * Input Format
+     * First and only argument is an integer denoting A.
+     *
+     *
+     *
+     * Output Format
+     * Return a two-dimensional array where each row in the returned array represents a row in the pattern.
+     *
+     *
+     *
+     * Example Input
+     * Input 1:
+     *
+     *  A = 3
+     * Input 2:
+     *
+     *  A = 4
+     *
+     *
+     * Example Output
+     * Output :1
+     *
+     *  [
+     *    [0, 0, 1]
+     *    [0, 2, 1]
+     *    [3, 2, 1]
+     *  ]
+     * Output 2:
+     *
+     *  [
+     *    [0, 0, 0, 1]
+     *    [0, 0, 2, 1]
+     *    [0, 3, 2, 1]
+     *    [4, 3, 2, 1]
+     *  ]
+     *
+     *
+     * Example Explanation
+     * Explanation 2:
+     *
+     *
+     *  For A = 4 pattern looks like:
+     *                                    1
+     *                                  2 1
+     *                                3 2 1
+     *                              4 3 2 1
+     *  So we will return it as two-dimensional array.
+     *  Row 1 contains 3 spaces and 1 integer so spaces will be considered as 0 in the output.
+     */
+    public int[][] printPattern(int limit) {
+        if(limit == 1) return new int[][] {{1}};
+
+        int[][] result = new int[limit][limit];
+        for(int i = 0; i > limit; i++ ) {
+            for(int j = limit-1; j >= limit-1+i; j-- ) {
+                result[i][j] = j;
+            }
+        }
+        return result;
+    }
+}
